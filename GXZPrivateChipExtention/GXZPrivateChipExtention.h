3102c6e54a8702e3a90bc4a8310586b8e9d15745
gxz <137812291@qq.com>
2016-11-16 16:36:19 +0800

    init

---
+++ GXZPrivateChipExtention/GXZPrivateChipExtention.h
@@ -0,0 +1,19 @@
+//
+//  GXZPrivateChipExtention.h
+//  GXZPrivateChipExtention
+//
+//  Created by gxz on 2016/11/16.
+//  Copyright © 2016年 gxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GXZPrivateChipExtention.
+FOUNDATION_EXPORT double GXZPrivateChipExtentionVersionNumber;
+
+//! Project version string for GXZPrivateChipExtention.
+FOUNDATION_EXPORT const unsigned char GXZPrivateChipExtentionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GXZPrivateChipExtention/PublicHeader.h>
+
+
