2578cb9a71d0be13ea1fba58a76d4b63bd659acc
gxz <137812291@qq.com>
2016-11-17 12:18:06 +0800

    ij

---
+++ GXZPrivateChipExtention StaticLibrary/GXZPrivateChipExtention_StaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  GXZPrivateChipExtention_StaticLibrary.h
+//  GXZPrivateChipExtention StaticLibrary
+//
+//  Created by gxz on 2016/11/17.
+//  Copyright © 2016年 gxz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GXZPrivateChipExtention_StaticLibrary : NSObject
+
+@end
---
+++ GXZPrivateChipExtention StaticLibrary/GXZPrivateChipExtention_StaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  GXZPrivateChipExtention_StaticLibrary.m
+//  GXZPrivateChipExtention StaticLibrary
+//
+//  Created by gxz on 2016/11/17.
+//  Copyright © 2016年 gxz. All rights reserved.
+//
+
+#import "GXZPrivateChipExtention_StaticLibrary.h"
+
+@implementation GXZPrivateChipExtention_StaticLibrary
+
+@end
